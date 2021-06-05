3564e1d6d8647d27bfb7dce01bfe9de4fc275f81
notomo <notomo.motono@gmail.com>
2021-06-06 08:13:38 +0900

    Echo extmark details

--- lua/nvimtool/command/extmark.lua
+++ lua/nvimtool/command/extmark.lua
@@ -3,10 +3,9 @@ local M = {}
 function M.echo_by_ns(name, nsid)
   local bufnr = vim.api.nvim_get_current_buf()
   local line = vim.fn.line(".") - 1
-  local marks = vim.api.nvim_buf_get_extmarks(bufnr, nsid, {line, 0}, {line, -1}, {})
+  local marks = vim.api.nvim_buf_get_extmarks(bufnr, nsid, {line, 0}, {line, -1}, {details = true})
   for _, mark in ipairs(marks) do
-    local id, row, col = unpack(mark)
-    local text = ("%s: mark_id=%d pos=[%d, %d]"):format(name, id, row, col)
+    local text = name .. " = " .. vim.inspect(mark, {newline = " ", indent = ""})
     vim.api.nvim_echo({{text}}, true, {})
   end
 end
